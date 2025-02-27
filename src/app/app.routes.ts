bbaf1f53c0aac56a9930a7abbfba7922e1708c94
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-02-26 21:22:42 -0300

    configura rotas para todas as páginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { ProjetosComponent } from './projetos/projetos.component';
+import { TecnologiasComponent } from './tecnologias/tecnologias.component';
+import { ContatoComponent } from './contato/contato.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sobre', component: SobreComponent },
+  { path: 'projetos', component: ProjetosComponent },
+  { path: 'tecnologias', component: TecnologiasComponent },
+  { path: 'contato', component: ContatoComponent },
+  { path: '**', redirectTo: '' }, // Rota padrão para páginas não encontradas
+];
