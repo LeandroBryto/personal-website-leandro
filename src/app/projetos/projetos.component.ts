072575d1999b07a756c47217b1a77edd302b74e9
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-02-27 20:10:40 -0300

    projeto no GitHub

--- src/app/projetos/projetos.component.ts
+++ src/app/projetos/projetos.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projetos',
@@ -10,7 +10,46 @@ import { CommonModule } from '@angular/common';
 })
 export class ProjetosComponent {
   projetos = [
-    { nome: 'Projeto 1', descricao: 'Descrição do Projeto 1', link: 'https://github.com/seu-usuario/projeto1' },
-    { nome: 'Projeto 2', descricao: 'Descrição do Projeto 2', link: 'https://github.com/seu-usuario/projeto2' },
+    {
+      nome: 'Portfólio Pessoal',
+      descricao: 'Meu site pessoal desenvolvido com Angular 17, destacando minhas habilidades e projetos.',
+      tecnologias: ['Angular', 'TypeScript', 'HTML', 'CSS'],
+      data: 'Fevereiro 2025',
+      status: 'Em andamento',
+      link: 'https://github.com/LeandroBryto/personal-website-leandro',
+    },
+    {
+      nome: 'Microsserviços Spring Boot - Ecommerce Microservices Primavera',
+      descricao: 'Projeto composto por dois microsserviços principais: User Service e Email Service. O objetivo é gerenciar usuários e enviar e-mails de confirmação usando RabbitMQ para comunicação e SMTP para envio de e-mails.',
+      tecnologias: [
+        'Spring Boot',
+        'Spring Data JPA',
+        'RabbitMQ',
+        'Spring Validation',
+        'Swagger',
+        'MySQL',
+        'Slf4j / Logback',
+        'Spring Mail',
+        'JavaMailSender'
+      ],
+      data: 'Janeiro 2025',
+      status: 'Concluído',
+      link: 'https://github.com/LeandroBryto/microservices-spring-boot'
+    },
+    {
+      nome: 'Desafio Backend do BTG Pactual',
+      descricao: 'Projeto desenvolvido como parte do desafio técnico do BTG Pactual, utilizando tecnologias modernas para implementação de uma API robusta e escalável.',
+      tecnologias: [
+        'Java 21',
+        'Spring Boot',
+        'Spring Data MongoDB',
+        'RabbitMQ',
+        'Docker'
+      ],
+      data: 'Janeiro 2025',
+      status: 'Concluído',
+      link: 'https://github.com/LeandroBryto/orderms'
+    }
   ];
+  
 }
